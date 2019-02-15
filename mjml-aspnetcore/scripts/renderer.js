d8e010853261c9c87ecbfa91e917259190d96342
John Knoll <jpknoll@gmail.com>
2019-02-15 10:17:34 -0800

    use explicit js object for return

---
+++ mjml-aspnetcore/scripts/renderer.js
@@ -0,0 +1,16 @@
+﻿const mjml2html = require('mjml');
+
+module.exports = function (callback, view, options) {
+    console.log('parsing view');
+    const result = mjml2html(view, options);
+    console.log(result);
+
+    if (result.errors && result.errors.length) {
+        callback({ errors: result.errors }, {});
+        return;
+    }
+
+    callback(null, {
+        html: result.html,
+    });
+};
