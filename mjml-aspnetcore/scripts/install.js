3cd5dc5a02c2de7afd76eca6722cfe2321c5b45c
John Knoll <jpknoll@gmail.com>
2019-02-15 14:15:58 -0800

    install project direct

--- mjml-aspnetcore/scripts/install.js
+++ mjml-aspnetcore/scripts/install.js
@@ -3,7 +3,7 @@ const cp = require('child_process');
 var options = { cwd: './scripts' };
 
 module.exports = function (callback) {
-    cp.exec('npm install', options, (err, stdout, stderr) => {
+    cp.exec('npm install mjml', options, (err, stdout, stderr) => {
         if (err) {
             callback(err, false);
             return;
