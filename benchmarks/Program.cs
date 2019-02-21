492fc2c5701b1c0ad50fd8881c722b35d9a62916
John Knoll <jpknoll@gmail.com>
2019-02-21 15:32:37 -0800

    add benchmark project

---
+++ benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+using test;
+
+namespace benchmarks
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Rendering>();
+        }
+    }
+}
